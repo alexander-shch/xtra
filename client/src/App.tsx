8e01a04f1a117542cdb4f9d8a258f6f04e6be119
Alexander Scherbakov <alexander.sh.sa@gmail.com>
2020-08-04 15:45:49 +0200

    add tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { setUserLog } from './Redux/userReduser/user.actions';
+import SignIn from './pages/SignIn/SignIn';
+import Main from './pages/Main/Main';
+import { Route } from 'react-router-dom';
+import Spinner from './componnent/spinner/Spinner';
+
+const App = ({ userLoged, setUserLog, loading }: any) => {
+  useEffect(() => {
+    setUserLog();
+  }, [setUserLog]);
+
+  return (
+    <Route
+      path='/'
+      render={() => (userLoged ? <Main /> : loading ? <Spinner /> : <SignIn />)}
+    ></Route>
+  );
+};
+
+const mapStateToProps = (state: any) => ({
+  userLoged: state.user.userLoged,
+  loading: state.user.loading,
+});
+
+const mapDispatchToProps = (dispatch: any) => ({
+  setUserLog: () => dispatch(setUserLog()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
