e2f730b33fb2d5edc8d626334b4e9d3779e849a7
Lior414 <ngrin.lior@gmail.com>
2020-11-18 21:41:28 +0200

    added footer component and implemented it in main component

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer.scss';
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <div className='container'>
+                <div className='copyright'>©  2020 כל הזכויות שמורות לאקסטרא</div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/footer';
 import { Switch, Route } from 'react-router-dom';
 import Dashboard from '../../components/dashboard/Dashboard';
 import SettingsRoutes from '../../components/Routes/setting-routes/settingsRoutes';
@@ -14,6 +15,7 @@ const Main = () => {
         <Route path='/lecturers' component={LecturersRoutes} />
         <Route path='/settings' component={SettingsRoutes} />
       </Switch>
+      <Footer />
     </>
   );
 };
